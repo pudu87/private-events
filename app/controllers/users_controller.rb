9dd07d84ec0fe5775429e2f4d1dc6de4ed9ec692
pudu87 <sam.cappelle87@gmail.com>
2020-07-15 19:14:08 -0400

    add usercontroller and userviews

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+  
+end
